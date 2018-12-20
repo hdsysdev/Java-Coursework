1c0dfe00203b68360263cdfd21b8f5cd5d4ee50f
Unknown <hdudowicz@gmail.com>
2018-12-20 23:35:30 +0000

    a commit

--- src/bcu/s17119577/adventure/game/CommandParser.java
+++ src/bcu/s17119577/adventure/game/CommandParser.java
@@ -1,6 +1,8 @@
 package bcu.s17119577.adventure.game;
 
-import bcu.changeme.adventure.effects.Effect;
+import bcu.s17119577.adventure.effects.Effect;
+import bcu.s17119577.adventure.effects.LookLocation;
+import bcu.s17119577.adventure.model.Item;
 
 public class CommandParser {
 	public static final String HELP_MESSAGE =
@@ -16,6 +18,32 @@ public class CommandParser {
 	
 	public static Effect parsePlayerCommand(String command) {
 		// TODO: complete implementation of parsePlayerCommand
-		return null;
+		switch (command){
+			case "look": return new LookLocation().execute();
+			case "look [item]":
+		}
+//		BufferedReader keyboard = new BufferedReader(
+////				new InputStreamReader(System.in)
+////		);
+////
+////		System.out.println("Address book");
+////		while(true) {
+////			System.out.print("> ");
+////			String command = keyboard.readLine();
+////			if("exit".equalsIgnoreCase(command)) {
+////				break;
+////			}
+////
+////			try {
+////				parseAndExecute(command);
+////			} catch(AlreadyPresentException ex) {
+////				System.out.println("The entry for " + ex.getName() + " already exists.");
+////			} catch(NotPresentException ex) {
+////				System.out.println("The entry for " + ex.getName() + " does not exist.");
+////			} catch(InvalidCommandException ex) {
+////				System.out.println("Invalid command (enter 'help' to see the valid commands).");
+////			}
+////		}
+
 	}
 }
---
+++ src/bcu/s17119577/adventure/game/Main.java
@@ -0,0 +1,9 @@
+package bcu.s17119577.adventure.game;
+
+public class Main {
+
+
+    public static void main(String[] args)  {
+
+    }
+}
